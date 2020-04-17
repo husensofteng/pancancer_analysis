10fec8015cb2e5a611c64f4e85d13fb552b6d5cb
Karolina Smolinska <karolina.smolinska@icm.uu.se>
2020-04-17 18:29:05 +0200

    Fix

--- ProcessCohorts.py
+++ ProcessCohorts.py
@@ -212,11 +212,11 @@ def get_sig_merged_elements_oncodrive(unified_mutation_input_files, mutation_inp
     fsep = '\t'
     awk_stmt_mut = """awk 'BEGIN{{FS=OFS="{fsep}"}}{{print $1,$2,$4,$5,$8,$6}}' {infile} |
     sort -k1,1n -k2,2n | uniq -u | awk 'BEGIN{{FS=OFS="\t"}}{{gsub("23","X", $1); gsub("24","Y", $1); gsub("chr","", $1); print $0}}' > {mutation_file}""".format(
-                                                fsep=fsep,  infile=mutation_input_files, variants_file=mutation_file_oncodrive+'_tmp')
+                                                fsep=fsep,  infile=mutation_input_files, mutation_file=mutation_file_oncodrive+'_tmp')
     os.system(awk_stmt_mut)
     #add header
     awk_stmt_mut2 = """echo "CHROMOSOME\tPOSITION\tREF\tALT\tSAMPLE\tCANCER_TYPE" | cat - {infile} > {mutation_file}""".format(
-                                                 infile=mutation_file_oncodrive+'_tmp', variants_file=mutation_file_oncodrive)
+                                                 infile=mutation_file_oncodrive+'_tmp', mutation_file=mutation_file_oncodrive)
     os.system(awk_stmt_mut2)
     
     os.remove(mutation_file_oncodrive +'_tmp')
@@ -227,12 +227,12 @@ def get_sig_merged_elements_oncodrive(unified_mutation_input_files, mutation_inp
     filter_cond = 'if($6>1)' #remove elements with one mutation
     awk_stmt_elem = """awk 'BEGIN{{FS=OFS="{fsep}"}}{{{filter_cond} {{print $1,$2,$3,$15}}}}' {infile} |
     sort -k1,1n -k2,3n | uniq -u | awk 'BEGIN{{FS=OFS="\t"}}{{gsub("23","X", $1); gsub("24","Y", $1); print $0}}' > {element_file}""".format(
-                                                fsep=fsep, filter_cond= filter_cond, infile=merged_muts_output_file, variants_file=element_file_oncodrive+'_tmp')
+                                                fsep=fsep, filter_cond= filter_cond, infile=merged_muts_output_file, element_filevariants_file=element_file_oncodrive+'_tmp')
     os.system(awk_stmt_elem)
 
     #add header
     awk_stmt_elem2 = """echo "CHROMOSOME\tSTART\tEND\tELEMENT" | cat - {infile} > {element_file}""".format(
-                                                 infile=element_file_oncodrive+'_tmp', variants_file=element_file_oncodrive)
+                                                 infile=element_file_oncodrive+'_tmp', element_filevariants_file=element_file_oncodrive)
     os.system(awk_stmt_elem2)
     
     os.remove(element_file_oncodrive +'_tmp')
