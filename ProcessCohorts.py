1c908cb222212c9e61ff50362b6c8300555c9210
Karolina Smolinska <karolina.smolinska@icm.uu.se>
2020-04-17 18:53:21 +0200

    Fix

--- ProcessCohorts.py
+++ ProcessCohorts.py
@@ -210,11 +210,11 @@ def get_sig_merged_elements_oncodrive(unified_mutation_input_files, mutation_inp
     '''Prepare mutation file'''
     mutation_file_oncodrive = mutation_input_files + '_oncodrive'
     fsep = '\t'
-    awk_stmt_mut = """awk 'BEGIN{{FS=OFS="{fsep}"}}{{print $1,$2,$4,$5,$8,$6}}' {infile} |
-    sort -k1,1n -k2,2n | uniq -u | awk 'BEGIN{{FS=OFS="\t"}}{{gsub("23","X", $1); gsub("24","Y", $1); gsub("chr","", $1); print $0}}' > {mutation_file}""".format(
+    awk_stmt_mut = """awk 'BEGIN{{FS=OFS="{fsep}"}}{{print $1,$2,$4,$5,$8,$6}}' {infile} | sort -k1,1n -k2,2n | uniq -u | awk 'BEGIN{{FS=OFS="\t"}}{{gsub("23","X", $1); gsub("24","Y", $1); gsub("chr","", $1); print $0}}' > {mutation_file}""".format(
                                                 fsep=fsep,  infile=mutation_input_files, mutation_file=mutation_file_oncodrive+'_tmp')
     print(awk_stmt_mut)
     os.system(awk_stmt_mut)
+    
     #add header
     awk_stmt_mut2 = """echo "CHROMOSOME\tPOSITION\tREF\tALT\tSAMPLE\tCANCER_TYPE" | cat - {infile} > {mutation_file}""".format(
                                                  infile=mutation_file_oncodrive+'_tmp', mutation_file=mutation_file_oncodrive)
@@ -223,11 +223,10 @@ def get_sig_merged_elements_oncodrive(unified_mutation_input_files, mutation_inp
     os.remove(mutation_file_oncodrive +'_tmp')
     
     '''Prepare elements file'''
-    elements_file_oncodrive = merged_muts_output_file + '_oncodrive'
-
+    element_file_oncodrive = merged_muts_output_file + '_oncodrive'
+    
     filter_cond = 'if($6>1)' #remove elements with one mutation
-    awk_stmt_elem = """awk 'BEGIN{{FS=OFS="{fsep}"}}{{{filter_cond} {{print $1,$2,$3,$15}}}}' {infile} |
-    sort -k1,1n -k2,3n | uniq -u | awk 'BEGIN{{FS=OFS="\t"}}{{gsub("23","X", $1); gsub("24","Y", $1); print $0}}' > {element_file}""".format(
+    awk_stmt_elem = """awk 'BEGIN{{FS=OFS="{fsep}"}}{{{filter_cond} {{print $1,$2,$3,$15}}}}' {infile} | sort -k1,1n -k2,3n | uniq -u | awk 'BEGIN{{FS=OFS="\t"}}{{gsub("23","X", $1); gsub("24","Y", $1); print $0}}' > {element_file}""".format(
                                                 fsep=fsep, filter_cond= filter_cond, infile=merged_muts_output_file, element_file=element_file_oncodrive+'_tmp')
     os.system(awk_stmt_elem)
     print(awk_stmt_elem)
