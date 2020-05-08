5502004ed7d45e1c15643d29b972e75e089b640d
karsmo <32451075+karsmo@users.noreply.github.com>
2020-05-08 16:45:49 +0200

    Add number of cores argument

--- ActiveDriverWGS_to_run.R
+++ ActiveDriverWGS_to_run.R
@@ -3,22 +3,28 @@ library(tidyverse)
 library(data.table)
 #args
 args = commandArgs(trailingOnly=TRUE)
+#elements file
 df_elem = args[1]
+#mutations file
 df_mut = args[2]
+#min number of mutations in element
 n_mut_ele = args[3]
+#output file
 out_file = args[4]
+#number of cores
+n_cores = args[5]
 
 print(df_elem)
 print(n_mut_ele)
 #elements
-data_my <- read.csv(df_elem, sep = '\t', header = TRUE, skip = 6)
+data_my <- read.csv(df_elem, sep = '\t', header = FALSE)
 head(data_my) %>%  print
 
 #at least 2 mutations in an element
-data_my_2mut <- data_my[which(data_my$X.Muts >= n_mut_ele),] 
+data_my_2mut <- data_my[which(data_my[,6] >= n_mut_ele),] 
 
 #elements to test
-data_elem <- data_my_2mut[,c(1:4)] 
+data_elem <- data_my_2mut[,c(1:3,13)] 
 colnames(data_elem) <- c('chr',
                          'start',
                          'end',
@@ -40,9 +46,10 @@ mut_in$chr <- gsub('24','Y',mut_in$chr)
 mut_in$pos1 <- as.numeric(mut_in$pos1)
 mut_in$pos2 <- as.numeric(mut_in$pos2)
 result = ActiveDriverWGS(mutations = mut_in,
-                         elements = data_elem, mc.cores = 3)
+                         elements = data_elem, mc.cores = n_cores)
 
-colnames(result)[1] = 'Position'
-result_join <- left_join(data_my,result, by= 'Position' ) %>%  dplyr::select(-c(element_muts_obs:site_enriched, fdr_site:has_site_mutations))
-colnames(result_join)[(length(result_join[1,])-1):length(result_join[1,])] <- c('ElementPval_ActiveDriver',   'ElementFDR_ActiveDriver')
-write.table(result_join, file = out_file,sep='\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
+colnames(result)[1] = 'id'
+colnames(data_my)[13] = 'id'
+result_join <- left_join(data_my,result, by= 'id' ) %>%  dplyr::select(-c(element_muts_obs:site_enriched, fdr_site:has_site_mutations))
+#colnames(result_join)[(length(result_join[1,])-1):length(result_join[1,])] <- c('ElementPval_ActiveDriver',   'ElementFDR_ActiveDriver')
+write.table(result_join, file = out_file,sep='\t', col.names = FALSE, row.names = FALSE, quote = FALSE, na = 'NA')
