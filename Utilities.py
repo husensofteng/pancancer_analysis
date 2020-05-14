acaadbb71441d9012a815e7546f0413520d99a6f
Karolina Smolinska <karolina.smolinska@icm.uu.se>
2020-05-14 20:17:07 +0200

    sorted = False

--- Utilities.py
+++ Utilities.py
@@ -635,7 +635,7 @@ def get_scores_per_window(observed_input_file_obj, tmp_dir, tmp_dir_intersect,
         simulated_input_file_obj = BedTool(simulated_input_file_sorted) 
         #print(simulated_input_file_obj)               
         #intersect the simulated file with the observed mutation file. Provide a sum of f_score and motif breaking score
-        observed_input_file_obj.intersect(simulated_input_file_obj, wo = True, sorted = True).saveas(simulated_input_file_tmp_overallTFs)
+        observed_input_file_obj.intersect(simulated_input_file_obj, wo = True).saveas(simulated_input_file_tmp_overallTFs)
         window_id_fscroe_file = """awk 'BEGIN{{FS=OFS="\t"}}{{if ($16==".") print $6,$16; else print $6,$16+$17}}' {simulated_input_file_tmp_overallTFs} > {simulated_input_file_tmp_overallTFs_local}""".format(simulated_input_file_tmp_overallTFs=simulated_input_file_tmp_overallTFs, simulated_input_file_tmp_overallTFs_local=simulated_input_file_tmp_overallTFs_local)
         os.system(window_id_fscroe_file)
         
