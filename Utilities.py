a6edfbc9601b238036e308aefb7d2757fd16420d
Karolina Smolinska <karolina.smolinska@icm.uu.se>
2020-05-17 16:15:34 +0200

    Fix

--- Utilities.py
+++ Utilities.py
@@ -623,6 +623,7 @@ def get_scores_per_window(observed_input_file_obj, tmp_dir, tmp_dir_intersect,
         count = len(open(simulated_input_file).readlines(  ))
         count2 = len(open(simulated_ifile_pos_temp).readlines(  ))
         if(count != count2):
+            print('ok')
             awk_tmp =r"""awk 'BEGIN{{FS=OFS="\t"}}{{ printf ("%s\t%d\t%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32)}}' {sim_ifile} > {sim_ofile} """.format(sim_ifile = simulated_input_file, sim_ofile = simulated_input_file_position)
             os.system(awk_tmp)
             simulated_input_file = simulated_input_file_position 
