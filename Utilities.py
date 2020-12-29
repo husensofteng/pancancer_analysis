aaf95b87803d2601b5e57c876396cd119942a0f4
Karolina Smolinska <karolina.smolinska@icm.uu.se>
2020-12-29 15:50:13 +0100

    Fix

--- Utilities.py
+++ Utilities.py
@@ -380,7 +380,12 @@ def assess_stat_elements_local_domain(observed_input_file, simulated_input_file,
                 line_number+=1
                 l = observed_infile.readline().strip().split('\t')
         print('observed_input_file: ', observed_input_file_temp_file_per_chr)
-        observed_input_file_obj = BedTool(observed_input_file_temp_file_per_chr)
+        
+        observed_input_file_temp_file_per_chr_sort=observed_input_file_temp_file_per_chr+'_sort'
+
+        if not os.path.exists(observed_input_file_temp_file_per_chr_sort):
+            os.system("""sort -k1,1n -k2,2n {} > {}""".format(observed_input_file_temp_file_per_chr_sort))
+        observed_input_file_obj = BedTool(observed_input_file_temp_file_per_chr_sort)
         
         
         simulated_input_file_sort=simulated_input_file+'_sort'
@@ -389,6 +394,7 @@ def assess_stat_elements_local_domain(observed_input_file, simulated_input_file,
          
         if not os.path.exists(simulated_input_file_sort):
             os.system("""sort -k1,1n -k2,2n {} > {}""".format(simulated_input_file,simulated_input_file_sort))
+            
             
         simulated_input_file_temp = simulated_input_file+"_temp"
         observed_input_file_obj.map(BedTool(simulated_input_file_sort), c=4, o=['collapse'], g='/proj/snic2020-16-50/nobackup/pancananalysis/pancan12Feb2020/cancer_datafiles/chr_order_hg19.txt').saveas(simulated_input_file_temp)
