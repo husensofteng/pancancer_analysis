0b0c9106ad114ec60ea5ddccf77894d9517ac1c8
Karolina Smolinska <karolina.smolinska@icm.uu.se>
2021-01-01 17:01:30 +0100

    Fix

--- ProcessCohorts.py
+++ ProcessCohorts.py
@@ -433,7 +433,7 @@ def run_cohort(cohort, created_cohorts, mutation_input_files, mutations_cohorts_
     sig_muts_file_tmp=sig_muts_file+"_tmp"
         
     BedTool(active_driver_output_file_sig_tmp).intersect(BedTool(created_cohorts[cohort][0]), wb=True).saveas(sig_muts_file_tmp)
-    os.system("""awk 'BEGIN{{FS=OFS="\t"}}{{print $22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53}}' {} | sort -k1,1 -k2,2n -V | uniq -u >{}""".format(sig_muts_file_tmp,sig_muts_file))
+    os.system("""awk 'BEGIN{{FS=OFS="\t"}}{{print $22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53}}' {} | sort -k1,1 -k2,2n -V | uniq  >{}""".format(sig_muts_file_tmp,sig_muts_file))
         
         
     sig_muts_per_tf_mutation_input_files=[]
